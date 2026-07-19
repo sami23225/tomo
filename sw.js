3bb7d3243bbddd46eb4901513e5fc620f6916657
sami23225 <sami23225@icloud.com>
2026-07-19 16:02:14 -0400

    Update cache version from v33 to v34

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tomo-v33';
+const CACHE='tomo-v34';
 const ASSETS=['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k!==CACHE?caches.delete(k):null))).then(()=>self.clients.claim()));});
